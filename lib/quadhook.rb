693d874bf5fbda601a2b2ecf97fb1cbafac5e3a4
Pat Allan <pat@freelancing-gods.com>
2015-04-21 15:24:41 +1000

    Requiring dependencies.

--- lib/quadhook.rb
+++ lib/quadhook.rb
@@ -1,3 +1,10 @@
+require 'base64'
+require 'openssl'
+require 'rack'
+require 'json'
+require 'active_support/notifications'
+require 'active_support/core_ext/module/delegation'
+
 module Quadhook
   #
 end
